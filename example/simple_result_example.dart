251052fb6e8eb573e744dc8352f7dcf2ac0ce753
Saulo Gatti <saulogatti@gmail.com>
2025-12-22 01:57:24 -0300

    first commit

---
+++ example/simple_result_example.dart
@@ -0,0 +1,6 @@
+import 'package:simple_result/simple_result.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/simple_result_test.dart
@@ -0,0 +1,16 @@
+import 'package:simple_result/simple_result.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
